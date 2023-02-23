ea42d42448a1bfe93dbcbf6812bd944a881257f2
Lucas Almendros <almendroslm@gmail.com>
2023-02-23 20:04:56 +0100

    accept command line parameters

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-go build -o booking cmd/web/*.go && ./booking
+go build -o booking cmd/web/*.go
+./booking -dbname=bookings -dbuser=root -cache=false -production=false -dbpass=toor
+# host=localhost port=5432 dbname=bookings user=root password=toor
